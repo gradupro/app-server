7e3bed9923f64b3f1e33d3ef616233b53378e7f2
정지원 <jeongjiwon@jeongjiwon-ui-MacBookPro.local>
2023-03-21 20:56:12 +0900

    3.21 back-end development
    - 보호자 요청 API 추가
    - 보호자 수락 API 추가
    - 보호자 목록 조회 API 추가

--- server/src/user/user.providers.ts
+++ server/src/user/user.providers.ts
@@ -1,10 +1,17 @@
 import { DataSource } from 'typeorm';
-import { User } from './entities/user.entity';
+import { User, UserProtector } from './entities/user.entity';
 
 export const userProviders = [
   {
     provide: 'USER_REPOSITORY',
     useFactory: (dataSource: DataSource) => dataSource.getRepository(User),
+    inject: ['DATA_SOURCE'],
+  },
+
+  {
+    provide: 'PROTECTOR_REPOSITORY',
+    useFactory: (dataSource: DataSource) =>
+      dataSource.getRepository(UserProtector),
     inject: ['DATA_SOURCE'],
   },
 ];
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -3,13 +3,15 @@ import { EntityNotFoundError, Repository } from 'typeorm';
 import { AuthService } from '../auth/auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
-import { User, userToken } from './entities/user.entity';
+import { User, UserProtector, userToken } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
   constructor(
     @Inject('USER_REPOSITORY')
     private userRepository: Repository<User>,
+    @Inject('PROTECTOR_REPOSITORY')
+    private protectorRepository: Repository<UserProtector>,
     private readonly authService: AuthService,
   ) {}
 
@@ -24,7 +26,10 @@ export class UserService {
         .insert()
         .values([createUserDto])
         .execute();
-      result.accessToken = await this.authService.createJWT(createdUser.raw.id);
+      console.log(createdUser);
+      result.accessToken = await this.authService.createJWT(
+        createdUser.raw.insertId,
+      );
       return result;
     } catch (e) {
       console.log(e);
@@ -84,6 +89,151 @@ export class UserService {
         })
         .getOne();
       return userInfo;
+    } catch (e) {
+      console.log(e);
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: [e.message.split('\n')[0]],
+          error: 'INTERNAL_SERVER_ERROR',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async requestProtector(userId: number, protectorId: number): Promise<any> {
+    try {
+      const user = await this.userRepository.findOne({
+        where: {
+          id: userId,
+        },
+        relations: {
+          protectors: true,
+        },
+      });
+
+      const protector = await this.userRepository.findOne({
+        where: {
+          id: protectorId,
+        },
+        relations: {
+          wards: true,
+        },
+      });
+
+      const userProtector = new UserProtector();
+      userProtector.protector = protector;
+      userProtector.ward = user;
+
+      user.protectors.push(userProtector);
+      protector.wards.push(userProtector);
+
+      await this.userRepository.save([user, protector]);
+      await this.protectorRepository.save([userProtector]);
+      return {
+        user: { name: user.name, id: user.id },
+        protector: { name: protector.name, id: protector.id },
+        accept: userProtector.accept,
+      };
+    } catch (e) {
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: [e.message.split('\n')[0]],
+          error: 'INTERNAL_SERVER_ERROR',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async getRequestedProtection(userId: number): Promise<any> {
+    try {
+      const requestedProtectionList = await this.protectorRepository
+        .createQueryBuilder('userProtector')
+        .leftJoin('userProtector.ward', 'WARD')
+        .leftJoin('userProtector.protector', 'PROTECTOR')
+        .addSelect('WARD.id')
+        .addSelect('WARD.name')
+        .addSelect('WARD.phone_number')
+        .where('PROTECTOR.id = :id', { id: userId })
+        .getMany();
+      return requestedProtectionList;
+    } catch (e) {
+      console.log(e);
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: [e.message.split('\n')[0]],
+          error: 'INTERNAL_SERVER_ERROR',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async allowRequestedProtection(userId: number, wardId: number): Promise<any> {
+    try {
+      console.log(userId, wardId);
+      await this.protectorRepository
+        .createQueryBuilder()
+        .update(UserProtector)
+        .set({ accept: true })
+        .where('user_protector.protectorId = :userId', { userId })
+        .andWhere('user_protector.wardId = :wardId', { wardId })
+        .execute();
+
+      const requestedProtection = await this.protectorRepository
+        .createQueryBuilder('userProtector')
+        .leftJoin('userProtector.ward', 'WARD')
+        .leftJoin('userProtector.protector', 'PROTECTOR')
+        .addSelect('WARD.id')
+        .addSelect('WARD.name')
+        .addSelect('WARD.phone_number')
+        .addSelect('PROTECTOR.id')
+        .addSelect('PROTECTOR.name')
+        .addSelect('PROTECTOR.phone_number')
+        .where('PROTECTOR.id = :userId', { userId })
+        .andWhere('WARD.id = :wardId', { wardId })
+        .getOneOrFail();
+      return requestedProtection;
+    } catch (e) {
+      console.log(e);
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: [e.message.split('\n')[0]],
+          error: 'INTERNAL_SERVER_ERROR',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async getProtectorList(wardId: number): Promise<any> {
+    try {
+      const protectorList = await this.protectorRepository.find({
+        relations: {
+          protector: true,
+        },
+        select: {
+          request_date: true,
+          accept: true,
+          protector: {
+            id: true,
+            name: true,
+            phone_number: true,
+          },
+        },
+        where: {
+          ward: {
+            id: wardId,
+          },
+          accept: true,
+        },
+      });
+      return protectorList;
     } catch (e) {
       console.log(e);
       throw new HttpException(
